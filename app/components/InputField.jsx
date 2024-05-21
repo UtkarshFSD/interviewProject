2156cf14b7c112acb3020db010119e0d47c80a40
UtkarshFSD <Utkarshrai707@gmail.com>
2024-05-21 17:08:07 +0530

    custoM input Field

---
+++ app/components/InputField.jsx
@@ -0,0 +1,155 @@
+"use client";
+import React, { useState } from "react";
+
+const InputField = ({
+  inputStyle,
+  inputTitle,
+  required,
+  disable,
+  customholderChanger,
+  customChangeFunction,
+  holder,
+  lablelstyle,
+  error,
+  maxLength,
+  minLength,
+  options,
+  currency,
+  regexPattern,
+  labelStyle,
+  type = "text",
+  validationPattern,
+}) => {
+  const [inputValue, setInputValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    console.log("nevalue", newValue);
+    if (validationPattern) {
+      setIsValid(validationPattern.test(newValue));
+    }
+
+    if (customChangeFunction) {
+      customChangeFunction("event", event.target.value);
+    }
+  };
+
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "$",
+    }).format(value);
+  };
+
+  const validateInput = () => {
+    if (required && inputValue.trim() === "") {
+      return false;
+    }
+    if (validationPattern && !validationPattern.test(inputValue)) {
+      return false;
+    }
+
+
+    if (
+      (maxLength && maxLength < inputValue.length) ||
+      (minLength && minLength > inputValue.length)
+    ) {
+     setIsValid(false)
+    }
+
+    return true;
+  };
+
+  const renderInput = () => {
+    switch (type) {
+      case "select":
+        return (
+          <select
+            style={inputStyle}
+            onChange={handleChange}
+            disabled={disable}
+            required={required}
+          >
+            {options.map(({ value, label }) => (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            ))}
+          </select>
+        );
+
+      case "currency":
+        return (
+          <input
+            type="text"
+            style={inputStyle}
+            placeholder={holder}
+            onChange={handleChange}
+            disabled={disable}
+            required={required}
+            maxLength={maxLength}
+            minLength={minLength}
+            value={formatCurrency(inputValue)}
+          />
+        );
+
+      case "radio":
+      case "checkbox":
+        return (
+          <div>
+            {options.map(({ value, label }) => (
+              <label key={value}>
+                <input
+                  type={type}
+                  value={value}
+                  onChange={handleChange}
+                  disabled={disable}
+                  required={required}
+                />
+              </label>
+            ))}
+          </div>
+        );
+
+      case "textarea":
+        return (
+          <textarea
+            style={inputStyle}
+            placeholder={holder}
+            onChange={handleChange}
+            disabled={disable}
+            required={required}
+            maxLength={maxLength}
+            minLength={minLength}
+          />
+        );
+      default:
+        return (
+          <input
+            type={type}
+            style={inputStyle}
+            placeholder={holder}
+            onChange={handleChange}
+            disabled={disable}
+            required={required}
+            maxLength={maxLength}
+            minLength={minLength}
+          />
+        );
+    }
+  };
+
+  return (
+    <div>
+      <label style={labelStyle}>{inputTitle}</label>
+      {renderInput()}
+      {!isValid && <div> {"Please Put Valid value"}</div>}
+      {!validateInput() && <div> {"Please Put Valid value"}</div>}
+    </div>
+  );
+};
+
+export default InputField;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,70 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import InputField from "./components/InputField";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  const options = [
+    { value: "value1", label: "label1" },
+    { value: "value2", label: "label2" },
+    { value: "value3", label: "label3" },
+    { value: "value4", label: "label4" },
+  ];
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleSubmit = () => {
+    console.log("inputValue::::", inputValue);
+  };
+
   return (
     <main className={styles.main}>
-    
+      <h3>Custom Input Field</h3>
+      <InputField
+        labelStyle={{ fontWeight: "500" }}
+        inputStyle={{ width: "300px", padding: "5px", borderWidth: "1px" }}
+        inputTitle="Input Field"
+        type="text"
+        holder="Your Name"
+        customChangeFunction={handleInputChange}
+        value={inputValue}
+        maxLength={50}
+        minLength={7}
+        required
+      />
+      <InputField
+        labelStyle={{ fontWeight: "500" }}
+        inputStyle={{ width: "300px", padding: "5px", borderWidht: "1px" }}
+        inputTitle="Select"
+        type="select"
+        holder="Your Options"
+        options={options}
+        customChangeFunction={handleInputChange}
+        value={inputValue}
+        required
+      />
+      <InputField
+        labelStyle={{ fontWeight: "500" }}
+        inputStyle={{
+          width: "300px",
+          padding: "5px",
+          borderWidht: "1px",
+          display: "flex",
+          gap: "5px",
+        }}
+        inputTitle="Radio / CheckBox"
+        type="checkbox"
+        holder="Your Options"
+        customChangeFunction={handleInputChange}
+        options={options}
+        value={inputValue}
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </main>
   );
 }
